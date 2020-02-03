774b877c074380c9feb9ef68715fd44efde041e9
Turas11 <wiktor.grzegorz.turczynski@gmail.com>
2020-02-03 14:01:17 +0100

    fiexs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="app">
-    <div class="nav">
-      <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
-    </div>
        <router-view />
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Montserrat:200,400,700&display=swap');
+  *{
+    box-sizing: border-box;
+
+  }
+  body {
+    font-family: 'Monsterrat', sans-serif;
+  }
 
 </style>
